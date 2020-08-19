a968b8bf9a5840977663d5d2cf032628425eec15
puneet <puneetrajwani@gmail.com>
2020-08-19 19:24:45 +0530

    Added new request url

--- routes/cors.js
+++ routes/cors.js
@@ -1,6 +1,6 @@
 var cors = require('cors')
 
-const whiteList =['http://localhost:3000','http://localhost:4200', 'http://localhost:4200/login']
+const whiteList =['http://localhost:3000','http://localhost:4200', 'http://localhost:4200/login', 'https://flamboyant-dijkstra-7e93d2.netlify.app']
 
 var corsOptionDelegate = (req,callback)=>{
     var corsOption;
